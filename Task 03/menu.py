2a1632ea31ef5838f2896824cd226d70097dab83
Heather Brand-Williamson <117382879+heatherbrandwilliamson@users.noreply.github.com>
2023-02-12 10:54:29 +0000

    Add files via upload

---
+++ Task 03/menu.py
@@ -0,0 +1,11 @@
+
+
+print("Menu: Chips, Pizza, Burger, Fish, Garlic Bread, Chicken ")
+
+item_1, item_2, item_3 = input("Enter your 3 favourite foods from the menu to order: ").split()
+
+print(item_1)
+print(item_2)
+print(item_3)
+print("Has been ordered!")
+
