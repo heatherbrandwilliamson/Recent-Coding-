8c2f4b3e99d3f527b394dc110c23d60b5d1c412a
Heather Brand-Williamson <117382879+heatherbrandwilliamson@users.noreply.github.com>
2023-02-12 11:02:25 +0000

    Initial commit

---
+++ Task 5 - The String Data Type/replace.py
@@ -0,0 +1,15 @@
+# declare and store variable
+
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!."
+
+# print variable
+print(sentence)
+
+# replace ! with blank space (This leaves a space before the fullstop, and I have not deleted)
+print(sentence.replace("!"," "))
+
+# reprint with uppercase
+print(sentence.replace("!"," ").upper())
+
+# reprint in reverse
+print(sentence[::-1])
---
+++ Task 5 - The String Data Type/strings.py
@@ -0,0 +1,6 @@
+
+# declare variable called hero
+hero = "$$$Superman$$$"
+
+# strip the $ from the variable and print
+print(hero.strip("$"))
